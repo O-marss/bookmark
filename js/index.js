46edf2aa2adfc0391b8a85c94c9a687ae03ebe69
0mar-cc <omarmoh3010@gmail.com>
2024-11-23 08:00:40 +0200

    reset folder name after submit

--- js/index.js
+++ js/index.js
@@ -172,6 +172,7 @@ function displayFolders(arr) {
 function reset() {
   siteUrlInput.value = null;
   siteNameInput.value = null;
+  addListInput.value = null;
 
   siteNameInput.classList.remove("is-valid");
   siteUrlInput.classList.remove("is-valid");
